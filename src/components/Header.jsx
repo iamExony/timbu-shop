20b152295b73df1308e235f9c81024de890028ca
Eze Onyemaechi <105824065+iamExony@users.noreply.github.com>
2024-07-10 06:52:36 +0100

    push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { MdShoppingCart } from "react-icons/md";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+}
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +21,7 @@ const Header = () => {
       <div className='flex gap-2 justify-center items-center'>
         <img src="images/logo.svg" alt="logo" className='hidden md:block' />
         <button onClick={toggleSidebar} className="block md:hidden hover:text-primary-color text-white font-bold py-2 px-4 rounded">
-          <img src="/images/bread-icon.svg" alt="cart" />
+          <img src="/images/bread-icon.svg" alt="menu" />
         </button>
         <div className="hidden text-2xl font-semibold font-inter text-blackColor md:block md:text-white">Timbu</div>
       </div>
@@ -23,14 +29,29 @@ const Header = () => {
       {/* Mobile view title */}
       <div className="text-2xl font-semibold font-inter text-blackColor md:text-white md:hidden">Timbu</div>
 
-      {/* Navbar */}
-      <nav className="hidden md:flex md:space-x-4 font-pop">
-        <Link to="/" className="p-1 hover:border-b-2 hover:border-white">Home</Link>
-        <a href='#product' className="p-1 hover:border-b-2 hover:border-white">Product</a>
-        <a href="#contact" className="p-1 hover:border-b-2 hover:border-white">Contact</a>
+      {/* Navbar Desktop*/}
+      <nav 
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+        className="hidden md:flex md:space-x-4 font-pop"
+      >
+        <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }} >
+          <Link to="/" className="p-1 hover:border-b-2 hover:border-white">Home
+        </Link>
+        </motion.button>
+        <motion.a
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        href='#product' className="p-1 hover:border-b-2 hover:border-white">Product</motion.a>
+        <motion.a
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }} 
+        href="#contact" className="p-1 hover:border-b-2 hover:border-white">Contact</motion.a>
       </nav>
       <Link to="check" className="block md:hidden hover:text-primary-color text-white font-bold py-2 px-4 rounded">
-      <MdShoppingCart className='text-black text-3xl' />
+        <MdShoppingCart className='text-black text-3xl' />
       </Link>
       <button className="hidden md:flex md:items-center md:gap-2 bg-lightPink hover:bg-[#F5E5FF] hover:text-primary-color hover:font-semibold text-white font-normal py-2 px-4 rounded">
         <span>Get Started</span>
@@ -38,10 +59,11 @@ const Header = () => {
       </button>
 
       {/* Sidebar */}
-      <div
-        className={`fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out md:hidden`}
+      <motion.div
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+        className="fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out md:hidden"
       >
         <div className="flex justify-between items-center p-4">
           {/* Logo */}
@@ -65,19 +87,29 @@ const Header = () => {
           </button>
         </div>
 
-        {/* Navbar Items */}
+        {/* Navbar Items mobile*/}
         <nav className="flex flex-col p-4 text-headingColor text-lg font-normal">
-          <Link to="/" className="py-2 px-4 hover:bg-red-500 hover:text-white">
+          <motion.button className="text-left py-2 px-4 hover:bg-red-500 hover:text-white"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}>
+            <Link to="/" >
             Home
           </Link>
-          <a href="#product" className="py-2 px-4 hover:bg-red-500 hover:text-white">
+          </motion.button>
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }} 
+          href="#product" className="py-2 px-4 hover:bg-red-500 hover:text-white">
             Product
-          </a>
-          <a href="#contact" className="py-2 px-4 hover:bg-red-500 hover:text-white">
+          </motion.a>
+          <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }} 
+          href="#contact" className="py-2 px-4 hover:bg-red-500 hover:text-white">
             Contact
-          </a>
+          </motion.a>
         </nav>
-      </div>
+      </motion.div>
       {/* Sidebar End */}
     </header>
   );
