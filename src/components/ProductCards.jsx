81aa365bc359eef2a2116cc8cc18f7394b92ef48
Eze Onyemaechi <105824065+iamExony@users.noreply.github.com>
2024-07-10 08:18:06 +0100

    push

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import Modal from './Modal';
 import { products } from './data';
 import { MdShoppingCart } from "react-icons/md";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+}
 
 
 
@@ -21,7 +27,11 @@ const ProductCards = () => {
     <div className="flex flex-wrap gap-6 items-center justify-center">
       {products.map((product) => (
         <div key={product.id} className="w-full sm:w-1/2 md:w-1/4 px-3 mb-6">
-          <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+          <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }} 
+          variants={variants}            
+          className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
             <div className="w-full h-56 mb-4">
               <img src={product.image} alt={product.title} className="w-full h-full object-cover rounded" />
             </div>
@@ -33,7 +43,7 @@ const ProductCards = () => {
             ><MdShoppingCart />
               <span>Add to Cart</span>
             </button>
-          </div>
+          </motion.div>
         </div>
       ))}
     </div>
